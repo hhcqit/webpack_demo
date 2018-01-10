f191e91f0714631a19a9ee458d96826f19f528f1
hhcqit <hon_ghuan1688@126.com>
2018-01-10 17:15:07 +0800

    ss

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,56 @@ const path = require('path');
 const webpack = require('webpack');//访问内置插件
 const uglifyPlugin = require('uglifyjs-webpack-plugin');//不需要安装用来压缩文件的
 const htmlPlugin = require("html-webpack-plugin");//需要通过npm安装
+const extractTextPlugin =require("extract-text-webpack-plugin");//css分离插件
+
+var website ={
+    publicPath:"http://192.168.0.119:8080/"//公共路径
+}
 module.exports = {
     entry: {//入口
         entry: "./src/entry.js"
     },
     output: {//出口
         path: path.resolve(__dirname, 'dist'),
-        filename: "[name].js"//[name]表示打包出来的文件和入口名称相同
+        filename: "[name].js",//[name]表示打包出来的文件和入口名称相同
+        publicPath:website.publicPath//解决静态路径问题
     },
     module: {
         rules: [
             {
                 test: /\.css$/,//正则找到css文件
-                // use:['style-loader','css-loader'],//使用文件
-                // loader:['style-loader','css-loader'],//使用文件
-                use: [
-                    { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: false
-                        }
-                    }
-                ],
-                // query: //额外的配置项
+                use: extractTextPlugin.extract({//css分离工具规则配置
+                    fallback: "style-loader",
+                    use: "css-loader"
+                  })
             },
+            // {
+            //     test: /\.css$/,//正则找到css文件
+            //     use:['style-loader','css-loader'],//使用文件
+            // },
+            // {
+            //     test: /\.css$/,//正则找到css文件
+            //     loader:['style-loader','css-loader'],//使用文件
+            // },
+            // {
+            //     test: /\.css$/,//正则找到css文件
+            //     use: [
+            //         { loader: "style-loader" },
+            //         {
+            //             loader: "css-loader",
+            //             options: {
+            //                 modules: false
+            //             }
+            //         }
+            //     ],
+            //     // query: //额外的配置项
+            // },
             {
                 test:/\.(png|jpg|gif)/,
                 use:[{
                     loader:'url-loader',
                     options:{
-                        limit:500000 //图片小于limit时打成base64位
+                        limit:5000//图片小于limit时打成base64位
                     }
                 }]
             }
@@ -46,7 +65,8 @@ module.exports = {
             },
             hash:true,//去掉缓存
             template:'./src/index.html'
-        })
+        }),
+        new extractTextPlugin("/css/index.css")
     ],//插件
     devServer: {//开发服务
         contentBase: path.resolve(__dirname, 'dist'),//默认webpack dev server是从项目的根目录提供服务
